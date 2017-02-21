1e4372c85c7d567aa36633a76f070347cfeb1557
Carlos Martín <alu0100702293@ull.edu.es>
2017-02-21 20:06:04 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if(sudo service apache2 start)then
+if(sudo service apache2 start || sudo service lighttpd start)then
 	sudo chown -R www-data *
 elif(sudo service httpd start)then
 	sudo chown -R httpd *
