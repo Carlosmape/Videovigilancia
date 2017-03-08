be3a92e55f7dbf05a1f1e768933e10a73282c7a6
Camape <cagrmape@gmail.com>
2017-03-08 14:09:28 +0000

    Now you can download it to everywhere

--- configure.sh
+++ configure.sh
@@ -16,29 +16,17 @@ sudo apt-get install lighttpd
 #adding php support
 sudo lighty-enable-mod fastcgi 
 sudo lighty-enable-mod fastcgi-php
-<<<<<<< HEAD
 
-#Check all
-if(sudo service lighttpd force-reload)then
-	sudo chown -R www-data *
-	lighttpd_log="Passed"
-else
-	lighttpd_log="Error"
-
-=======
-clear
->>>>>>> 0016cb76b6e0226ee1b3b9483ac37d63ca21e948
 if(sudo cp -r * /var/www/html/)then
 	echo "#CAMS copied to /var/www/html/"
 fi
+
 if(sudo service lighttpd force-reload)then
-<<<<<<< HEAD
-	sudo chown -R www-data *
-=======
 	sudo chown -R www-data /var/www/html/*
 	echo "#CAMS has been configured!. Go to http://yoursite.com/install to do final step"
->>>>>>> 0016cb76b6e0226ee1b3b9483ac37d63ca21e948
+
 fi
+
 clear
 echo "#Motion...${motion_log}"
 echo "#Lighttpd...${lighttpd_log}"
