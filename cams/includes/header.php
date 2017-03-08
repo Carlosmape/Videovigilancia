1e684ed424fe8b8493f04dbe82dd7ea268a7f6b8
Camape <cagrmape@gmail.com>
2017-03-08 12:07:31 +0000

    Added favicon to CAMS

--- cams/includes/header.php
+++ cams/includes/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/svg" href="includes/favicon.svg"/>
     <title><?php echo TITLE;?>|Panel</title>
 
     <!-- Bootstrap -->
